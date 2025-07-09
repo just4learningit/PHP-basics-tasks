f088897b7e385d97fd32969efc9e6d3d42a2a0c5
just4learningit <jin.4working@gmail.com>
2025-07-09 11:43:50 +0900

    array_reverse()を使わない仕様に変更

--- lesson4/problem5.php
+++ lesson4/problem5.php
@@ -2,7 +2,12 @@
   $string = "しんぶんし";
 
   $array = mb_str_split($string);
-  $rev_array = array_reverse($array);
+  // $rev_array = array_reverse($array);
+  $rev_array = [];
+  for ($i = count($array) - 1; $i >= 0; $i--){
+    $rev_array[] = $array[$i];
+  }
+
   $rev_string = implode("", $rev_array);
 
   if ($string === $rev_string){
